6437f0e8cd3e123468bde1a964eee14d2be54493
Fedi Mhadhbi <111863722+Fedi-Mhadhbi@users.noreply.github.com>
2026-09-04 18:47:15 +0200

    Add files via upload

---
+++ service_worker.js
@@ -0,0 +1,111 @@
+const CACHE_NAME = "mizan-v1";
+
+const FILES_TO_CACHE = [
+    "./",
+    "./index.html",
+    "./login.html",
+    "./register.html",
+    "./dashboard.html",
+    "./transactions.html",
+    "./budget.html",
+    "./goals.html",
+    "./settings.html",
+
+    "./manifest.json",
+
+    "./css/style.css",
+
+    "./js/firebase.js",
+    "./js/auth.js",
+    "./js/dashboard.js",
+    "./js/transactions.js",
+    "./js/budget.js",
+    "./js/goals.js",
+    "./js/settings.js",
+    "./js/i18n.js",
+    "./js/common.js",
+
+    "./icons/icon-192.png",
+    "./icons/icon-512.png",
+    "./icons/icon-180.png"
+];
+
+
+self.addEventListener(
+    "install",
+    event => {
+
+        event.waitUntil(
+
+            caches.open(CACHE_NAME)
+                .then(
+                    cache =>
+                        cache.addAll(
+                            FILES_TO_CACHE
+                        )
+                )
+
+        );
+
+        self.skipWaiting();
+
+    }
+);
+
+
+self.addEventListener(
+    "activate",
+    event => {
+
+        event.waitUntil(
+
+            caches.keys()
+                .then(
+                    keys =>
+                        Promise.all(
+
+                            keys
+                                .filter(
+                                    key =>
+                                        key !==
+                                        CACHE_NAME
+                                )
+                                .map(
+                                    key =>
+                                        caches.delete(
+                                            key
+                                        )
+                                )
+
+                        )
+                )
+
+        );
+
+        self.clients.claim();
+
+    }
+);
+
+
+self.addEventListener(
+    "fetch",
+    event => {
+
+        event.respondWith(
+
+            caches.match(
+                event.request
+            )
+            .then(
+                cachedResponse =>
+
+                    cachedResponse ||
+                    fetch(event.request)
+
+            )
+
+        );
+
+    }
+);
